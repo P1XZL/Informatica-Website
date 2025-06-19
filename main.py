dcc241fe95fc0495d78b15b285a3f4fd456d65bd
Pixel <94076564+P1XZL@users.noreply.github.com>
2025-06-19 18:05:59 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template("/index.html")
+
+
+@app.route('/mods')
+def mods():
+    return render_template("/mods.html")
+
+
+@app.route('/gallery')
+def gallery():
+    return render_template("/gallery.html")
+
+
+@app.route('/textures')
+def textures():
+    return render_template("/textures.html")
+
+
+@app.route('/worlds')
+def worlds():
+    return render_template("/worlds.html")
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
